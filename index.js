9aaee86072824bb1276ec449d66143f2c40d567b
Olutola Oloruntobi Paul <oloruntobiolutola@gmail.com>
2023-09-08 23:31:06 +0100

    removed description

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-/*
-Given a string used to know the types of fingerprint data that needs to be collected in a certain kind of 
-enrolment system, write a method to validate the string based on the following rules:
-    1.  Each finger type should be represented with the keys shown in the Appendix at the bottom of this note 
-        (The keys should be separated by commas).
-    2.  For every finger type from the right hand present in the string, its left hand finger equivalent  must 
-        also be present (E.g. If a right thumb is present, a left thumb should also be present. 
-        If a right index is present, a left index should also be present, etc).
-    3.  An empty string is also valid.
-*/
-
 const isFingerprintTypesValid = (fingerPrintInputString) => { 
     if(fingerPrintInputString.length === 0) return true;
     
